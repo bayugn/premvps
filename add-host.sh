ef39a208e875b0fd114b1e0cc4945f57332ac7ef
gilagajet <84836081+gilagajet@users.noreply.github.com>
2021-07-11 01:46:23 +0800

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
 
 read -p "Input New Domain : " domainbaru
 
@@ -16,10 +12,6 @@ fi
 cat > /etc/v2ray/domain << END
 $domainbaru
 END
-
-sed -i 's/$MYIP/$domainbaru/g' /etc/openvpn/client-tcp-ssl.ovpn 
-sed -i 's/$MYIP/$domainbaru/g' /etc/openvpn/client-tcp-1194.ovpn 
-sed -i 's/$MYIP/$domainbaru/g' /etc/openvpn/client-udp-2200.ovpn 
 
 clear 
 echo "SUCCESS"
